554f3adf37bd5714414b3a07268e576cd0077856
Lawani-EJ <elyonjohn60@gmail.com>
2024-05-08 12:00:40 +0100

    Performing other operations with different types In this commit I've performed product  calculations to the monthly_savings and num months storing the result in year savings Printed the type for the year savings Calculated the sum of intro and intro and then stored the result in a new variable called doubleintro Printed the output.

---
+++ script.py
@@ -0,0 +1,16 @@
+monthtly_savings = 10
+num_months = 12
+intro = "Hello! How are You?"
+
+#Calculate the year_savings using monthly_savings and num_months
+year_savings = monthtly_savings*num_months
+print(year_savings)
+
+#Print the type of year_savings
+print(type(year_savings))
+
+#Assign the sum of intro and intro to doubleintro
+doubleintro = intro + intro
+
+#Print the doubleIntro
+print(doubleintro)
